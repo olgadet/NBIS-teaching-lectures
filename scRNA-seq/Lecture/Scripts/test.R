94cdd86c7772475c5cf493b1dde5d8b308c51b68
olgadh <olga.dethlefsen@nbis.se>
2018-05-18 14:37:20 +0200

    scRNA-seq 2018

---
+++ scRNA-seq/Lecture/Scripts/test.R
@@ -0,0 +1,14 @@
+rm(list=ls())
+load("testData.RData")
+
+idx.tumor <- which(data.group=="Tumor")
+idx.normal <- which(data.group=="Brain")
+data.counts <- as.matrix(data.counts)
+idx.0 <- which(apply(data.counts, 1, sum)==0)
+
+data.subset <- data.counts[-idx.0, ]
+hist(data.subset[1:10, idx.tumor], n=100)
+hist(data.subset[1:10, idx.normal], n=100)
+
+
+load("../DE/mast_data.Rdata")
